b9aa0bfe470357bb9d59d5b703f67608e3359087
zxcFRESKO <burhonidin2009@gmail.com>
2024-12-19 16:31:44 +0300

    исправление багов и убрал лишнее

--- js/contacts.js
+++ js/contacts.js
@@ -1,5 +1,3 @@
-const moon = document.querySelector('.moon')
-const sun = document.querySelector('.sun')
 const menu = document.querySelector('.logo-menu')
 const menu_ul = document.querySelector('.menu_ul')
 const open_btn = document.querySelector('.open_modal')
@@ -9,23 +7,6 @@ const modal_wrap = document.querySelector('.modal_wrap')
 const send_btn = document.querySelector('.send')
 const inputs = document.querySelectorAll('.input')
 
-moon.addEventListener('click', Moon)
-
-function Moon(){
-    let body = document.querySelector('body') 
-    moon.classList.add('hide');
-    sun.classList.remove('hide')
-    body.classList.add('night')
-}
-sun.addEventListener('click', Sun)
-
-function Sun(){
-    let body = document.querySelector('body') 
-    sun.classList.add('hide')
-    moon.classList.remove('hide')
-    body.classList.remove('night')
-    
-}
 document.addEventListener('mouseover', (event) => {
     if (event.target.classList.contains('logo-menu')) {
         menu_ul.classList.add('_active');
